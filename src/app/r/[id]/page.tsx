b2491842a4a8c59282fa7c291d8c918183869b28
Henri Cavalcante <hnri_mxel@hotmail.com>
2024-01-03 13:32:05 -0300

    Initial NextJS template

---
+++ src/app/r/[id]/page.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+import facebookSvg from "@/images/Facebook.svg";
+import twitterSvg from "@/images/Twitter.svg";
+import googleSvg from "@/images/Google.svg";
+import Input from "@/shared/Input/Input";
+import ButtonPrimary from "@/shared/Button/ButtonPrimary";
+import Image from "next/image";
+import Link from "next/link";
+
+
+
+const PageLogin = () => {
+  return (
+    <div className={`nc-PageLogin`} data-nc-id="PageLogin">
+     teste
+    </div>
+  );
+};
+
+export default PageLogin;
