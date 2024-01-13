ff0c768f5447f626aad2740f76c00bb8906ec5c6
Ewerton <ewertoncesar91@gmail.com>
2024-01-13 07:12:49 -0300

    sorting the list alphabetically

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { Category, CategoryApi } from "../../../luugoapi";
-import {CATEGORY} from "@/data/categories";
+import { CATEGORY } from "@/data/categories";
 
 const PageLogin: FC = async () => {
     const categoryApi = new CategoryApi();
@@ -17,6 +17,9 @@ const PageLogin: FC = async () => {
             subcategories.push(item);
         }
     });
+    
+    categories.sort((a, b) => CATEGORY[a.id].title.localeCompare(CATEGORY[b.id].title));
+    subcategories.sort((a, b) => CATEGORY[a.id].title.localeCompare(CATEGORY[b.id].title));
 
     return (
         <div className="relative py-10 lg:py-32 px-10">
@@ -26,19 +29,18 @@ const PageLogin: FC = async () => {
                     {categories.map((category) => (
                         <li key={category.id} className="py-2 px-10">
                             <a href={`/category/${category.id}`} className="transition-colors duration-300
-                            ease-in-out hover:text-blue-500">
+                ease-in-out hover:text-blue-500">
                                 {CATEGORY[category.id].title}
                             </a>
                             <ul>
                                 {subcategories
-                                    .filter((subcategory) => subcategory.parentCategoryId ===
-                                        category.id)
+                                    .filter((subcategory) => subcategory.parentCategoryId === category.id)
                                     .map((subcategory) => (
                                         <li key={subcategory.id} className="text-neutral-500
-                                        dark:text-neutral-400 text-sm">
+                      dark:text-neutral-400 text-sm">
                                             <a href={`/category/${subcategory.id}`} className="text-neutral-500
-                                            dark:text-neutral-400 text-sm transition-colors duration-300
-                                            ease-in-out hover:text-blue-500">
+                        dark:text-neutral-400 text-sm transition-colors duration-300
+                        ease-in-out hover:text-blue-500">
                                                 {CATEGORY[subcategory.id].title}
                                             </a>
                                         </li>
