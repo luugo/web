ca83095c30f41583b024d49d3276dc1d723270ba
Ewerton <ewertoncesar91@gmail.com>
2024-12-10 10:10:50 -0300

    fixing change password request

--- src/app/(accounts)/account-password/page.tsx
+++ src/app/(accounts)/account-password/page.tsx
@@ -3,9 +3,16 @@ import Label from "@/components/Label/Label";
 import React, {useState} from "react";
 import ButtonPrimary from "@/shared/Button/ButtonPrimary";
 import Input from "@/shared/Input/Input";
-import {AuthenticationApi} from "../../../../luugoapi";
+import { Alert } from "@/shared/Alert/Alert";
+import {AuthenticationApi, AuthenticationPostDefaultResponse} from "../../../../luugoapi";
+import {AlertOptions} from "@/interfaces";
+import {useRouter} from "next/navigation";
 
 const AccountPass = () => {
+  const router = useRouter();
+  const [alert, setAlert] = useState('');
+  const [typeAlert, setTypeAlert] = useState<keyof AlertOptions>('success');
+  const [isShowAlert, setShowAlert] = useState<boolean>(false);
   const [currentPassword, setCurrentPassword] = useState<string>('');
   const [newPassword, setNewPassword] = useState<string>('');
   const [confirmNewPassword, setConfirmNewPassword] = useState<string>('');
@@ -16,24 +23,45 @@ const AccountPass = () => {
   let storageData: any = null;
   if (typeof window !== 'undefined') {
     storageData = localStorage.getItem('luugo');
-  };
+  }
 
-  const handleUpdatePassword = async () => {
+  const showAlert = (msg: string, type: keyof AlertOptions = 'success') => {
+    setAlert(msg);
+    setTypeAlert(type);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 5000);
+  }
+
+  const handleUpdatePassword = async (e: React.FormEvent) => {
+    e.preventDefault();
     if (storageData !== null) {
-      let storage = JSON.parse(storageData)
+      JSON.parse(storageData);
       const requestParameters: any = {
-        emailVerificationKey: storage?.user?.authenticationId,
         currentPassword: currentPassword,
         newPassword: newPassword,
         confirmNewPassword: confirmNewPassword,
-      }
+      };
+
       try {
-        return await authApi?.authenticationEmailPost({
-          authenticationEmailPostRequest: requestParameters,
-        });
+        const luugo: AuthenticationPostDefaultResponse = JSON.parse(storageData);
+
+        await authApi?.authenticationPut(
+          {authenticationPutRequest: requestParameters},
+          {
+            headers: {
+              Authorization: `Bearer ${luugo?.token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        showAlert('Senha alterada com sucesso!');
+
       } catch (error: any) {
         const errorData = await error.response?.json();
-        if(errorData) {
+        if (errorData) {
           const message = errorData[0]?.message
           if (message == null) {
             setErrorMessage("Erro inesperado. Por favor, tente novamente.");
@@ -43,49 +71,54 @@ const AccountPass = () => {
         }
       }
     }
-  }
+  };
 
   return (
     <div className="space-y-10 sm:space-y-12">
-      {/* HEADING */}
-      <h2 className="text-2xl sm:text-3xl font-semibold">
-        Atualize sua Senha
-      </h2>
-      <div className=" max-w-xl space-y-6">
-        <div>
-          <Label>Senha Atual</Label>
-          <Input
-            type="password"
-            className="mt-1.5"
-            value={currentPassword}
-            onChange={(e) => setCurrentPassword(e?.target?.value)}
-          />
-        </div>
-        <div>
-          <Label>Nova Senha</Label>
-          <Input
-            type="password"
-            className="mt-1.5"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e?.target?.value)}
-          />
-        </div>
-        <div>
-          <Label>Confirmar Senha</Label>
-          <Input
-            type="password"
-            className="mt-1.5"
-            value={confirmNewPassword}
-            onChange={(e) => setConfirmNewPassword(e?.target?.value)}
-          />
-          {errorMessage && (
-              <p className="block text-red-500 text-sm font-bold mb-2">{errorMessage}</p>
-          )}
-        </div>
-        <div className="pt-2">
-          <ButtonPrimary onClick={handleUpdatePassword}>Atualizar Senha</ButtonPrimary>
-        </div>
+      <div className="fixed left-0 top-0 z-max w-full p-4">
+        {isShowAlert && (<Alert type={typeAlert} onClick={() => setShowAlert(false)}>{alert}</Alert>)}
       </div>
+      {/* HEADING */}
+      <form onSubmit={handleUpdatePassword} className="w-full max-w-md space-y-4">
+        <h2 className="text-2xl sm:text-3xl font-semibold">
+          Atualize sua Senha
+        </h2>
+        <div className=" max-w-xl space-y-6">
+          <div>
+            <Label>Senha Atual</Label>
+            <Input
+              type="password"
+              className="mt-1.5"
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e?.target?.value)}
+            />
+          </div>
+          <div>
+            <Label>Nova Senha</Label>
+            <Input
+              type="password"
+              className="mt-1.5"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e?.target?.value)}
+            />
+          </div>
+          <div>
+            <Label>Confirmar Senha</Label>
+            <Input
+              type="password"
+              className="mt-1.5"
+              value={confirmNewPassword}
+              onChange={(e) => setConfirmNewPassword(e?.target?.value)}
+            />
+            {errorMessage && (
+              <p className="text-red-500 font-semibold">{errorMessage}</p>
+            )}
+          </div>
+          <div className="pt-2">
+            <ButtonPrimary type={"submit"}>Atualizar Senha</ButtonPrimary>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
