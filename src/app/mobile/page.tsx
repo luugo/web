f5ff3253451e1a522d5b2e5fa4f4c6994e321db9
Ewerton <ewertoncesar91@gmail.com>
2024-12-18 11:45:05 -0300

    fixing /mobile

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -1,15 +1,22 @@
+// src/app/mobile/page.tsx
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default function MobileRedirect() {
-  const userAgent = typeof window !== "undefined" ? navigator.userAgent : "";
+  return null;
+}
+
+export async function generateMetadata({ headers }: { headers: Headers }) {
+  const userAgent = headers.get("user-agent") || "";
 
   if (/android/i.test(userAgent)) {
-    redirect("https://play.google.com/store/apps/details?id=br.com.luugo.app");
+    redirect("https://play.google.com/store/apps/details?id=com.seuapp");
   } else if (/iPad|iPhone|iPod/.test(userAgent)) {
-    redirect("/");
+    redirect("https://apps.apple.com/app/id123456789");
   } else {
     redirect("/");
   }
+
+  return {};
 }
-
-
