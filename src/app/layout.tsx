c2f9c20876e1428eec9a21ebc6191f150538fa64
Henri Cavalcante <hnri_mxel@hotmail.com>
2025-01-29 15:41:17 -0300

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import SiteHeader from "@/app/SiteHeader";
 import CommonClient from "./CommonClient";
 import { Providers } from "@/providers";
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -24,6 +25,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" dir="" className={poppins.className}>
+      <head>
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-23XR5GPX7N"
+            strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-23XR5GPX7N');
+        `}
+        </Script>
+      </head>
       <body className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
         <GoogleOAuthProvider clientId="637549763916-sa80ph8s7nsbpdhtnv3bf6a1q29s3fae.apps.googleusercontent.com">
           <Providers>
