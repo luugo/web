107e41354078e0ed7797eb40dd8eae40e1c1e458
Henri Cavalcante <hnri_mxel@hotmail.com>
2025-01-30 12:43:33 -0300

    Added analytics to redirect

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -1,18 +1,44 @@
-import { redirect } from "next/navigation";
-import { headers } from "next/headers";
-
-export const dynamic = "force-dynamic";
+"use client";
+import React, {useEffect} from "react";
+import SectionHero3 from "@/components/SectionHero/SectionHero3";
+import ButtonPrimary from "@/shared/Button/ButtonPrimary";
 
 export default function MobileRedirect() {
-  const requestHeaders = headers();
-  const userAgent = requestHeaders.get("user-agent") || "";
+    useEffect(() => {
+        if (typeof window === "undefined") return;
 
-  if (/android/i.test(userAgent)) {
-    redirect("https://play.google.com/store/apps/details?id=br.com.luugo.app");
-  } else if (/iPad|iPhone|iPod/.test(userAgent)) {
-    redirect("https://apps.apple.com/");
-  } else {
-    redirect("/");
-  }
+        const userAgent = navigator.userAgent || "";
+        let redirectUrl = "/";
+        let platform = "unknown";
+
+        if (/android/i.test(userAgent)) {
+            redirectUrl = "https://play.google.com/store/apps/details?id=br.com.luugo.app";
+            platform = "android";
+        } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+            redirectUrl = "https://apps.apple.com/";
+            platform = "ios";
+        }
+
+        if (typeof window.gtag === "function") {
+            window.gtag("event", "qr_code", {
+                event_category: "redirect",
+                event_label: platform,
+                value: 1,
+            });
+        }
+
+        setTimeout(() => {
+            window.location.href = redirectUrl;
+        }, 500);
+    }, []);
+
+    return (
+        <div>
+            <div className="container px-4 -bottom-4 -top-4">
+                <h5>
+                    Redirecionando ...
+                </h5>
+            </div>
+        </div>
+    );
 }
-
