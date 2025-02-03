4165d8610c289c9a518969146cc0c8f6002aebc5
Lucas Miqueias <contato@lucasmiqueias.com.br>
2025-02-03 17:32:13 -0300

    lazy loading

--- src/app/rentable/[id]/RentablePage.tsx
+++ src/app/rentable/[id]/RentablePage.tsx
@@ -1,128 +1,127 @@
 "use client";
 
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Prices from "@/components/Prices";
 import ModalViewAllReviews from "./ModalViewAllReviews";
 import RentableInfo from "@/components/RentableInfo";
-import {GoogleMap, LoadScript, Marker} from "@react-google-maps/api";
-import ImageGallery from "@/components/ImageGallery/ImageGallery";
-import {dataProduct} from "./page";
-import {UserContact} from "@api";
+import dynamic from "next/dynamic";
+import { dataProduct } from "./page";
+import { UserContact } from "@api";
+
+const ImageGallery = dynamic(
+  () => import("@/components/ImageGallery/ImageGallery"),
+  { ssr: false, loading: () => <p>Carregando imagens...</p> }
+);
+const GoogleMapComponent = dynamic(
+  () => import("@react-google-maps/api").then((mod) => mod.GoogleMap),
+  { ssr: false, loading: () => <p>Carregando mapa...</p> }
+);
+const LoadScriptComponent = dynamic(
+  () => import("@react-google-maps/api").then((mod) => mod.LoadScript),
+  { ssr: false }
+);
+const MarkerComponent = dynamic(
+  () => import("@react-google-maps/api").then((mod) => mod.Marker),
+  { ssr: false }
+);
 
 const ProductDetail = (dataProduct: dataProduct) => {
   const [isOpenModalViewAllReviews, setIsOpenModalViewAllReviews] =
     useState(false);
   const [processedDescription, setProcessedDescription] = useState("");
+  const [isClient, setIsClient] = useState(false);
 
-  const isValidCoordinate = (coord: { lat: any; lng: any }) => {
-    return (
-      typeof coord.lat === "number" &&
-      typeof coord.lng === "number" &&
-      isFinite(coord.lat) &&
-      isFinite(coord.lng)
-    );
-  };
-
-  const renderSectionContent = () => {
-    return (
-      <div className="space-y-7 2xl:space-y-8">
-        <div>
-          <h2 className="text-2xl sm:text-3xl font-semibold">
-            {dataProduct.title ?? ""}
-          </h2>
-
-          <div className="flex items-center mt-5 space-x-4 sm:space-x-5">
-            <Prices
-              contentClass="py-1 px-2 md:py-1.5 md:px-3 text-lg font-semibold"
-              price={dataProduct.price}
-              billingFrequency={dataProduct.billingFrequency}
-            />
-          </div>
-        </div>
-
-        <hr className=" 2xl:!my-10 border-slate-200 dark:border-slate-700"></hr>
-        <RentableInfo
-          {...(dataProduct.user.social as unknown as {
-            [key: string]: UserContact;
-          })}
-        />
-      </div>
-    );
-  };
-
-  const renderDetailSection = () => {
-    return (
-      <div className="">
-        <h2 className="text-2xl font-semibold">Descrição</h2>
-        <div className="prose prose-sm sm:prose dark:prose-invert sm:max-w-4xl mt-7">
-          <p
-            dangerouslySetInnerHTML={{
-              __html: processedDescription || "Descrição não disponível",
-            }}
-          ></p>
-        </div>
-      </div>
-    );
-  };
-
-  const handleRenderMap = () => {
-    if (
-      !isValidCoordinate({
-        lat: dataProduct.geolocation.y,
-        lng: dataProduct.geolocation.x,
-      })
-    ) {
-      return <p>Mapa não disponível no momento.</p>;
-    }
-
+  const renderHeader = () => {
     return (
       <>
-        <h2 className="text-2xl font-semibold">Localização</h2>
-        <LoadScript googleMapsApiKey="AIzaSyAMYZyR35_t_qG75PyL9JKDGHx_D05wAgc">
-          <GoogleMap
-            mapContainerStyle={{width: "100%", height: "600px"}}
-            center={{
-              lat: dataProduct.geolocation.y,
-              lng: dataProduct.geolocation.x,
-            }}
-            zoom={10}
-          >
-            <Marker
-              position={{
-                lat: dataProduct.geolocation.y,
-                lng: dataProduct.geolocation.x,
-              }}
-            />
-          </GoogleMap>
-        </LoadScript>
+        <h2 className="text-2xl pb-2 sm:text-3xl font-semibold">
+          {dataProduct.title ?? ""}
+        </h2>
+        <div className="flex items-center">
+          <Prices
+            contentClass="py-1 px-2 md:py-1.5 md:px-3 text-lg font-semibold"
+            price={dataProduct.price}
+            billingFrequency={dataProduct.billingFrequency}
+          />
+        </div>
       </>
     );
   };
 
   useEffect(() => {
+    setIsClient(true);
     if (dataProduct.description) {
       setProcessedDescription(dataProduct.description);
     }
   }, [dataProduct.description]);
   return (
-    <div className="pb-20 xl:pb-28 lg:pt-14">
+    <div className="pb-20 xl:pb-28">
       <div className={`nc-ProductDetailPage `}>
-        <main className="container mt-5 lg:mt-11">
-          <div className="grid gap-8 grid-cols-12">
-            <div className="lg:col-span-8 md:col-span-8 col-span-12">
-              <ImageGallery images={dataProduct.images}/>
+        <main className="container mt-1">
+          <div className="grid gap-6 grid-cols-12">
+            <div className="pt-6 lg:col-span-12 md:col-span-12 col-span-12">
+              {renderHeader()}
+            </div>
+            <div className="lg:col-span-12 md:col-span-8 col-span-12 rounded-lg overflow-hidden">
+              {isClient ? (
+                <ImageGallery images={dataProduct.images} />
+              ) : (
+                <p>Carregando imagens...</p>
+              )}
             </div>
             <div className="lg:col-span-4 md:col-span-4 col-span-12">
-              {renderSectionContent()}
+              <RentableInfo
+                {...(dataProduct.user.social as unknown as {
+                  [key: string]: UserContact;
+                })}
+              />
             </div>
-            <div className="lg:col-span-8 md:col-span-8 col-span-12">
-              {renderDetailSection()}
+            <div className="lg:col-span-8 md:col-span-8 col-span-12 radius-lg overflow-hidden">
+              <h2 className="text-2xl font-semibold">Descrição</h2>
+              <div className="prose prose-sm sm:prose dark:prose-invert sm:max-w-4xl mt-7">
+                {isClient ? (
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html:
+                        processedDescription || "Descrição não disponível",
+                    }}
+                  ></p>
+                ) : (
+                  <p>Carregando descrição...</p>
+                )}
+              </div>
             </div>
           </div>
 
           <div className="mt-12 sm:mt-16 space-y-10 sm:space-y-16">
-            <hr className="border-slate-200 dark:border-slate-700"/>
-            {handleRenderMap()}
+            <hr className="border-slate-200 dark:border-slate-700" />
+            <div>
+              <h2 className="text-2xl font-semibold">Localização</h2>
+              {isClient && dataProduct.geolocation ? (
+                <LoadScriptComponent
+                  googleMapsApiKey="AIzaSyAMYZyR35_t_qG75PyL9JKDGHx_D05wAgc"
+                  id={`${dataProduct.geolocation.y}:${dataProduct.geolocation.x}`}
+                >
+                  <GoogleMapComponent
+                    mapContainerStyle={{ width: "100%", height: "600px" }}
+                    center={{
+                      lat: dataProduct.geolocation.y,
+                      lng: dataProduct.geolocation.x,
+                    }}
+                    zoom={10}
+                  >
+                    <MarkerComponent
+                      position={{
+                        lat: dataProduct.geolocation.y,
+                        lng: dataProduct.geolocation.x,
+                      }}
+                    />
+                  </GoogleMapComponent>
+                </LoadScriptComponent>
+              ) : (
+                <p>Carregando mapa...</p>
+              )}
+            </div>
           </div>
         </main>
 
