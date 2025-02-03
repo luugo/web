2cf8541e7eb625ddf91d7ce4bfe67463dd4c51b0
Lucas Miqueias <contato@lucasmiqueias.com.br>
2025-02-03 17:30:32 -0300

    Viewport utils

---
+++ src/utils/useViewportSize.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const useViewportSize = () => {
+  const [size, setSize] = useState({
+    width: typeof window !== "undefined" ? window.innerWidth : 0,
+    height: typeof window !== "undefined" ? window.innerHeight : 0,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Para atualizar no primeiro render
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return size;
+};
+
+export default useViewportSize;
