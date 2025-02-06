a1fb153a418a6124a1ea1ac91baee6463546f18c
Henri Cavalcante <hnri_mxel@hotmail.com>
2025-02-05 21:57:28 -0300

    Deleted tab filter

--- src/components/HeaderFilterSearchPage.tsx
+++ src/components/HeaderFilterSearchPage.tsx
@@ -5,7 +5,6 @@ import Nav from "@/shared/Nav/Nav";
 import NavItem from "@/shared/NavItem/NavItem";
 import {ChevronDownIcon} from "@heroicons/react/24/outline";
 import ButtonPrimary from "@/shared/Button/ButtonPrimary";
-import TabFilters from "@/components/TabFilters";
 import {Transition} from "@/app/headlessui";
 
 export interface HeaderFilterSearchPageProps {
@@ -98,7 +97,6 @@ const HeaderFilterSearchPage: FC<HeaderFilterSearchPageProps> = ({
         leaveTo="opacity-0"
       >
         <div className="w-full border-b border-neutral-200/70 dark:border-neutral-700 my-8"></div>
-        <TabFilters/>
       </Transition>
     </div>
   );
--- src/components/HeaderFilterSection.tsx
+++ src/components/HeaderFilterSection.tsx
@@ -6,7 +6,6 @@ import Nav from "@/shared/Nav/Nav";
 import NavItem from "@/shared/NavItem/NavItem";
 import {ChevronDownIcon} from "@heroicons/react/24/outline";
 import ButtonPrimary from "@/shared/Button/ButtonPrimary";
-import TabFilters from "@/components/TabFilters";
 import {Transition} from "@/app/headlessui";
 
 export interface HeaderFilterSectionProps {
@@ -94,7 +93,6 @@ const HeaderFilterSection: FC<HeaderFilterSectionProps> = ({
         leaveTo="opacity-0"
       >
         <div className="w-full border-b border-neutral-200 dark:border-neutral-700 my-8"></div>
-        <TabFilters/>
       </Transition>
     </div>
   );
